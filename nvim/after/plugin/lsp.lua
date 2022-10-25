3d00f4ecc29f6906e98e9de11f2d3c56bf27f9cc
Sebastian Loose <sebastian.loose@experienceone.com>
2022-10-25 10:14:26 +0200

    Fix nvim lsp config after nvim 0.8 update

--- nvim/after/plugin/lsp.lua
+++ nvim/after/plugin/lsp.lua
@@ -5,7 +5,7 @@ local cmp = require 'cmp'
 
 -- Add additional capabilities
 local capabilities = vim.lsp.protocol.make_client_capabilities()
-capabilities = require('cmp_nvim_lsp').update_capabilities(capabilities)
+capabilities = require('cmp_nvim_lsp').default_capabilities(capabilities)
 
 -- Use an on_attach function to only map the following keys
 -- after the language server attaches to the current buffer
